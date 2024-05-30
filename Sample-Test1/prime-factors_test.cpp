12ed30a40329cce4f42fc2867f52c8c634e3e10b
HwangEyiKwon <hwang21035@naver.com>
2024-05-30 11:51:26 +0900

    [feature] TDD Cycle1

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -3,10 +3,8 @@
 
 using namespace std;
 
-TEST(TestCaseName, TestName) {
-    EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 1);
+TEST(PrimeFactors, Of1) {
+    PrimeFactor prime_factor;
+    vector<int> expected = {};
+    EXPECT_EQ(expected, prime_factor.of(1));
 }
