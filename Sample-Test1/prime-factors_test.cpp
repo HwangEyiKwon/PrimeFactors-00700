833cd73f543f44f06fbc78030edd9c417f07863a
HwangEyiKwon <hwang21035@naver.com>
2024-05-30 11:13:46 +0900

    commit test

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -5,5 +5,8 @@ using namespace std;
 
 TEST(TestCaseName, TestName) {
     EXPECT_EQ(1, 1);
-    EXPECT_TRUE(true);
+    EXPECT_EQ(1, 1);
+    EXPECT_EQ(1, 1);
+    EXPECT_EQ(1, 1);
+    EXPECT_EQ(1, 1);
 }
